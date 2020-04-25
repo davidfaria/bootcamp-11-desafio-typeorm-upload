89b7013cc2dd469b3ce07a834b982af21d80835f
David Faria <davidfaria89@gmail.com>
2020-04-25 09:43:35 -0300

    refactor: improve import csv routine

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,7 +1,8 @@
 import fs from 'fs';
+import { getRepository, In } from 'typeorm';
 import csvToJson from 'csvtojson';
 import Transaction from '../models/Transaction';
-import CreateTransactionService from './CreateTransactionService';
+import Category from '../models/Category';
 
 interface Request {
   importFile: string;
@@ -16,19 +17,63 @@ interface TransactionCsv {
 
 class ImportTransactionsService {
   async execute({ importFile }: Request): Promise<Transaction[]> {
+    const categoryRepository = getRepository(Category);
+    const transactionRepository = getRepository(Transaction);
+
     const transactionscsv: TransactionCsv[] = await csvToJson().fromFile(
       importFile,
     );
-    const transactions: Transaction[] = [];
-    const createTransactionService = new CreateTransactionService();
 
     // TODO ( MELHORAR PROCESSO PARA INSEIR EM MASSA )
     // NÃO UTILIZAR A REGRA DE NEGÓCIO DO SERVISE - createTransactionService
 
-    for (const item of transactionscsv) {
-      const transaction = await createTransactionService.execute(item);
-      transactions.push(transaction);
-    }
+    // for (const item of transactionscsv) {
+    //   const transaction = await createTransactionService.execute(item);
+    //   transactions.push(transaction);
+    // }
+
+    const csvCategories = transactionscsv.map(tx => tx.category);
+
+    /** Get category that exists in database */
+    const existsCategories = await categoryRepository.find({
+      where: { title: In(csvCategories) },
+    });
+    const dbFoundCategories = existsCategories.map(category => category.title);
+
+    /** Get unique category that will be save to database */
+    const categoriesToSaveDB = Array.from(
+      new Set(csvCategories.filter(item => !dbFoundCategories.includes(item))),
+    );
+
+    /**
+     *  (Bulk) save new categories from csv
+     */
+    const newCategoriesSaved = await categoryRepository.save(
+      categoriesToSaveDB.map(title => {
+        return {
+          title,
+        };
+      }),
+    );
+
+    const AllCategoriesDB = [...existsCategories, ...newCategoriesSaved];
+
+    const transactionsToSaveDB = transactionscsv.map(tx => {
+      return {
+        ...tx,
+        category: AllCategoriesDB.find(item => item.title === tx.category),
+      };
+    });
+
+    const transactions = await transactionRepository.save(transactionsToSaveDB);
+
+    // console.log(
+    //   csvCategories,
+    //   dbFoundCategories,
+    //   categoriesToSaveDB,
+    //   newCategoriesSaved,
+    //   transactions,
+    // );
 
     await fs.promises.unlink(importFile);
 
